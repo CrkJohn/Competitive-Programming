9205799df64ba89e9fd1d4f076d0473178d83e8b
CrkJohn <jhonhrge@gmail.com>
2018-12-22 22:19:42 -0500

    :=)

--- C++/Graph/10610.cpp
+++ C++/Graph/10610.cpp
@@ -4,7 +4,8 @@
 #define loop(i,a,b) for(int i = a ; i< b; i++)
 #define fors(i,a,b,s) for(int i=a;i<b;i=i+s)
 #define pb push_back
-
+#define fi first
+#define se second
 
 using namespace std;
 
@@ -16,8 +17,11 @@ using namespace std;
 
 typedef vector<int> vi;
 typedef vector<vi> vvi;
+typedef pair<double,int> di;
+typedef vector<di> vid;
+typedef vector<vid> vvid;
 
-vvi g;
+vvid g;
 
 
 const double EPS = 1e-9;
@@ -26,17 +30,37 @@ const int MAX_N = 1100;
 int dis[MAX_N];
 
 
-void bfs(int src,const int &n){
-	queue<int> q;
-	q.push(src);
-	FOR(i,n)dis[i] = -1;
-	dis[src] = 0;
-  while(!q.empty()){
-  }
-  return;
+typedef pair<double,double> dd;
+
+
+ostream & operator << (ostream &out, const dd &c){
+		out << "(" << c.fi << "," << c.se << ")\n";
+    return out;
 }
 
 
+
+
+int bfs(const double &max_d,const int &n){
+		queue<int> q;
+		q.push(0);
+		FOR(i,n)dis[i] = -1;
+		dis[0] = 0;
+		while (!q.empty()){
+			int u = q.front(); q.pop();
+			if(u==1)return dis[u]- 1;
+			//cerr << "u:" << u <<  endl;
+			loop(j, 0, (int)g[u].size()){
+				di v=g[u][j];
+			//cerr << "d:" << v.fi << " v:" << v.se << endl;
+				if (v.fi<max_d && dis[v.se] == -1){
+					dis[v.se]= dis[u] + 1;
+					q.push(v.se);
+				}
+		}
+	}
+	return -1;
+}
 
 int main(){
 	//lec();
@@ -44,8 +68,45 @@ int main(){
 	cin.tie(0);
   cout << fixed;
   cout.precision(5);
-	while(ntc--){
-
+	double v,m;
+	while(cin >> v >> m,m+v!=0.0){
+				vector< pair<double,double> > pts;
+				dd  src, tgt;
+				cin >> src.fi >> src.se;
+				cin >> tgt.fi >> tgt.se;
+        pts.pb(src);pts.pb(tgt);
+				string str;
+				getline(cin,str);
+				while(getline(cin,str), str != ""){
+						stringstream ss;
+						ss << str;
+            dd p;
+            ss >> p.fi >> p.se;
+            ss.clear();
+            pts.pb(p);
+				}
+				/*cerr << endl;
+				FOR(i,pts.size()){
+					cerr << pts[i].fi << " " << pts[i].se  << endl;
+				}**/
+				int lon = pts.size();
+        g.assign(lon,vid());
+        FOR(i,lon){
+						loop(j,i+1,lon){
+								dd  pta = pts[i];
+								dd  ptb = pts[j];
+                double V = sqrt(pow(pta.fi-ptb.fi,2) + pow(pta.se-ptb.se,2));
+                g[i].pb(di(V,j));
+                g[j].pb(di(V,i));
+					 }
+        }
+        double d = v*m*60.0;
+				int r = bfs(d,lon);
+				if(r !=-1){
+						cout << "Yes, visiting "<<r<<" other holes.\n";
+				}else{
+						cout << "No." << endl;
+				}
 	}
 	return 0;
 }
