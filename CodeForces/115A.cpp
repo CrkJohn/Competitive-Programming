c0bd800c843897db1feea19247bd75bfd80f28b1
John David Ibañez <JhonhRge@gmail.com>
2019-01-05 19:43:46 -0500

    Update 115A.cpp

--- CodeForces/115A.cpp
+++ CodeForces/115A.cpp
@@ -42,39 +42,38 @@ void dfs(const int &u,int depth){
 	vis[u] = 1;
 	if(depth>ans)ans = depth;
 	for(int v : g[u]){
-			if(!vis[v]){
-					dfs(v,depth+1);
-			}
+		if(!vis[v]){
+			dfs(v,depth+1);
+		}
 	}
 }
 
 
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.precision(10);
-    cout << fixed;
-    //in();
-    int n;
-    cin >> n;
-    for1(i,n){
-				cin >> 	p[i];
+	ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.precision(10);
+  cout << fixed;
+	//in();
+	int n;
+	cin >> n;
+	for1(i,n){
+	  cin >> 	p[i];
+	}
+	g.assign(n+1,vi());
+	for1(i,n){
+		if(p[i]!=-1){
+			g[p[i]].pb(i);
 		}
-		g.assign(n+1,vi());
-		for1(i,n){
-        if(p[i]!=-1){
-							g[p[i]].pb(i);
-        }
+	 }
+	ans  = 0; 
+	memset(vis,0,sizeof vis);
+	for1(i,n){
+	  if(!vis[i] && p[i]==-1){
+			dfs(i,1);
 		}
-		ans  = 0;
-		memset(vis,0,sizeof vis);
-		for1(i,n){
-				if(!vis[i] && p[i]==-1){
-						dfs(i,1);
-				}
-		}
-		cout << ans << endl;
-    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-    return 0;
+	 }
+	 cout << ans << endl;
+	 cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	 return 0;
 }
