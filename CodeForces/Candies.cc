29f8ca743be710b68431963b152f22fe66a09a55
CrkJohn <Jhonhrge@gmail.com>
2019-07-05 21:37:31 -0500

    CReate candy

---
+++ CodeForces/Candies.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define fore(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
+#define in() freopen("in.txt","r",stdin)
+#define out() freopen("out.txt","w",stdout)
+#define err() freopen("err.txt","w",stderr)
+#define cls(a,val) memset(a,val,sizeof a)
+#define INF  1e9;
+
+
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<ll, ll> pll;
+typedef double ld;
+
+template <typename T> ostream& operator<<(ostream& os, vector<T> v) {
+    os << "[ ";for(auto e : v) os << e << " ";
+    return os << "]";
+}
+ 
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+    cout.precision(10);
+    cout << fixed;
+#ifdef LOCAL
+    in();
+    err();
+#endif
+    int n;
+    cin >> n;
+    cin >> arr[n];
+    forn(i,n) cin >> arr[i];
+    csgt();
+    int q; 
+    cin >> q;
+    forn(query,q){
+        int ql,qr;
+        cout << query(1,1,n,ql,qr) << endl;
+    }
+
+
+#ifdef LOCAL
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+    return 0;
+} 
+
+
