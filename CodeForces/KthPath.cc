29e8103344268c2efb3f30a9a32a4a6c146fa334
CrkJohn <Jhonhrge@gmail.com>
2019-07-25 14:37:11 -0500

    Solve k-th shortest path on graph

--- CodeForces/KthPath.cc
+++ CodeForces/KthPath.cc
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-
+ 
 #define mp make_pair
 #define mt make_tuple
-#define X first
-#define Y second
+#define fi first
+#define se second
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
@@ -15,20 +15,20 @@
 #define out(x) freopen(#x".txt","w",stdout)
 #define err() freopen("err.txt","w",stderr)
 #define cls(a,val) memset(a,val,sizeof a)
-#define INF 3000000000000000007ll
+#define INF 1e18
 #define len(a) ((int)a.size())
-
-
+ 
+ 
 const double  eps =  1e-9;
 const int  MOD = 998244353; //1e9+7;
-
+ 
 inline int mul(int a, int b) { return 1ll*a*b%MOD; }
 inline int add(int a, int b) { return a + b < MOD ? a + b : a + b - MOD; }
  
  
-
+ 
 using namespace std;
-
+ 
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vpi;
@@ -37,24 +37,70 @@ typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
 typedef double ld;
 typedef pair<ld, ld> pld;
 typedef vector<string> vs;
-
+ 
 template<typename T> using minheap = priority_queue<T, vector<T>, greater<T>>;
 template<typename T> using maxheap = priority_queue<T, vector<T>, less<T>>;
 template <typename T> ostream& operator<<(ostream& os, vector<T> v){os << "[ ";for(auto e : v) os << e << " ";return os << "]";}
 template <typename T> ostream& operator<<(ostream& os, pair<T,T> p){os << "(" << p.fi << "," << p.se <<")";return os;}
-
+ 
 const double PI = acos(-1); 
 double DEG_to_RAD(double d) { return d * PI / 180.0; }
 double RAD_to_DEG(double r) { return r * 180.0 / PI; }
 double distance(double x,double y, double x_,double y_){return  sqrt(pow(x-x_,2) + pow(y-y_,2));}
 int isTriangle(int a, int b, int c){return (a+b>c && a+c>b && c+b>a);}
 ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
-
-
-
+ 
+ 
+struct edge{
+    ll u , v ,w; 
+    edge(ll U ,ll V ,ll W){
+        u = U;
+        v = V;
+        w  = W;
+    }
+    bool operator <(const edge other)const {
+        return w < other.w;
+    }
+};
+ 
+ 
+ 
+vector<vpll> G;
+vll ans;
+ll n,m,k;
+const int maxn = 2* 1e5 + 100;
+int exist[maxn];
+int nodeth =  1;
+    
+void dijkstra(ll s){
+    int debug = 30;
+    vll dis(nodeth+10);
+    priority_queue<pll, vector<pll>, greater<pll> > pq;
+    for(ll i = 0 ; i <=  nodeth; ++i) dis[i]=INF;  dis[s]=0;
+    pq.push(pll(0, s));    
+    while (!pq.empty()){
+        pll par=pq.top(); pq.pop();
+        ll d=par.fi , u=par.se;  
+        if (d<dis[u]) continue;
+        for( ll j = 0 ;  j <  len(G[u]) ; j++){
+            pll v=G[u][j];
+            if (dis[u]+v.se < dis[v.fi]){
+                dis[v.fi]= dis[u] + v.se;
+                pq.push(pll(dis[v.fi],v.fi));
+            }
+        }
+    }
+    for(ll i = s+1 ; i <= nodeth ; i++){
+        if(dis[i]!=0  && dis[i]!=INF && s<i){
+            ans.pb(dis[i]);
+        }
+    }
+}
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -65,81 +111,44 @@ int main() {
     in();
  //   err();
 #endif
-
-    int n , m  , k;
-    cin >> n>> m >> k;
-    vector< vector<pii> > adjList(n);
-
+    cin >> n >> m >> k;
+    vector<edge> edges;
     forn(i,m){
-        int u , v , w; 
-        cin >> u >> v >> w;
+        ll u , v , w; 
+        cin >> u >> v>> w;
         u--;
         v--;
-        adjList[u].pb(pii(v,w));
-
-        adjList[v].pb(pii(u,w));
+        edges.pb(edge(u,v,w));
     }
-    //define src
-    int tmp = n;
-    ll sum = 0 ;
-    fore(i.1,n){
-        sum += (i*n-1); 
-        if(sum>k){
-            s = i;
-            break;
-        }
+    sort(all(edges));
+    G.assign(n+1,vpll());
+    cls(exist,0);
+    map<int,int> idNode;
+    nodeth = 1;
+    forn(i,min(k,m)){
+        edge m = edges[i];
+        if(!idNode[m.v])idNode[m.v] = nodeth++; 
+        if(!idNode[m.u])idNode[m.u] = nodeth++;
+        G[idNode[m.u]].pb(pll(idNode[m.v],m.w));
+        G[idNode[m.v]].pb(pll(idNode[m.u],m.w));
+        exist[idNode[m.v]] = exist[idNode[m.u]] = 1;
     }
-
-
-    int maxn = 2* 1e5;
-    int vis[maxn];
-    memset(vis,0,sizeof vis);
-    vi dis2;
-    //forn(nodes,n){
-        vi dis(n);
-        priority_queue<pii, vector<pii>, less<pii> > pq;
-
-        int s = nodes;
-        forn(i, n) dis[i]=1e9;  
-        dis[s]=0;
-        while (!pq.empty()) pq.pop();
-        int d,u;
-        pq.push(pii(0, s));
-        while (!pq.empty()){
-            pii par=pq.top(); pq.pop();
-            d=par.X; u=par.Y;
-            if (d>dis[u]) continue;
-            forn(j,(int) adjList[u].size()){
-                pii v=adjList[u][j];
-                if (dis[u]+v.Y < dis[v.X]){
-                    dis[v.X]= dis[u] + v.Y;
-                    pq.push(pii(dis[v.X], v.X));
-                }
-            }
-        }
-      //  forn(i,n){
-      //      if(nodes != i && !vis[dis[i]]){
-      //          dis2.pb(dis[i]);
-      //          vis[dis[i]] = 1;
-      //      }
-       // }
-    //}
-    /*vi ans;
-    for(auto n : dis2){
-        ans.pb(n);
-    } */
-
+    ans.clear();
+    for(ll i = 1 ; i <= nodeth ; ++i){
+        //cerr << i << " dij " << endl;
     
-    sort(all(dis2));    
-    cout << dis2 << endl;
-    cout << dis2[k] << endl;
+        if(exist[i])dijkstra(i);
+        //cerr << i << " after dij " << endl;
     
-
-
+    }   
+    sort(all(ans));
+    //cout << ans << endl;
+    cout << ans[k-1] << endl;
+ 
 #ifdef LOCAL
     cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
 #endif
     return 0;
 } 
-
-
+ 
+ 
--- CodeForces/gen.py
+++ CodeForces/gen.py
@@ -1,3 +1,4 @@
+"""
 def criba_eratostenes(n):
   multiplos = set()
   for i in range(2, n+1):
@@ -6,3 +7,49 @@ def criba_eratostenes(n):
       multiplos.update(range(i*i, n+1, i))
 
 criba_eratostenes(10000020)
+"""
+
+from matplotlib import pyplot as plt
+from networkx import nx
+
+import random
+random.seed('Me gusta algo...')
+
+def randomGraphW(n,m,directed=True):
+    nxG = nx.gnm_random_graph(n,m,directed=directed,
+                             seed=random.getrandbits(20))
+    G = [[] for i in range(n)]
+    W = [[] for i in range(n)]
+    
+    for i,j in nxG.edges:
+        print(i+1,j+1 , end = " ")
+        G[i].append(j)
+        w = random.randrange(80231,893320068)
+        c = random.randrange(1,100)
+        print(w)
+        W[i].append(w)
+        if not directed:
+            G[j].append(i)
+            W[j].append(w)
+    return G, W
+
+def showGraph(G, W=None):
+    n = len(G)
+    E = [(i,j) for i in range(n) for j in G[i]]
+    nxG = nx.Graph(E)
+    pos = nx.spring_layout(nxG)
+    nx.draw(nxG,pos,with_labels=True,node_color='yellow',
+           node_size=500)
+    if W is not None:
+        labels = [W[i][k] for i in range(n) for k in range(len(G[i]))]
+        for (i,j),w in zip(E,labels):
+            nxG[i][j]['weight'] = w
+        labels = nx.get_edge_attributes(nxG,'weight')
+        nx.draw_networkx_edge_labels(nxG, pos, edge_labels=labels)
+    plt.show()
+    return
+
+n = 100000
+m = 200000
+print(n,m,400)
+G,W = randomGraphW(n,m, directed=False)
